a10be18903c5a4bbd96703e468cecba492cd0996
Glebanister <glebmar2001@gmail.com>
2022-04-04 23:28:39 +0300

    Update deploy script

--- ua_help/bot_students/run_release.sh
+++ ua_help/bot_students/run_release.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-bash -c 'conda run -n ukraine-help python main.py --root /root/ukraine-help/ua_help/bot_students/ --config /root/ukraine-help/ua_help/bot_students/resources/config_release.json'
+bash -c 'conda run -n ukraine-help python main.py --root ~/ukraine-help/ua_help/bot_students/ --config ~/ukraine-help/ua_help/bot_students/resources/config_release.json'
--- ua_help/bot_students/run_test.sh
+++ ua_help/bot_students/run_test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-bash -c 'python main.py --root /root/ukraine-help/ua_help/bot_students/ --config /root/ukraine-help/ua_help/bot_students/resources/config_test.json'
+bash -c 'python main.py --root ~/ukraine-help/ua_help/bot_students/ --config ~/ukraine-help/ua_help/bot_students/resources/config_test.json'

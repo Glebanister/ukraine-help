0ea4b9d7c8a014c218705eb1ff83322236b7fb65
Glebanister <glebmar2001@gmail.com>
2022-04-04 23:51:22 +0300

    Update port

--- ua_help/telegram/telegram_bot.py
+++ ua_help/telegram/telegram_bot.py
@@ -61,7 +61,7 @@ class TelegramBot:
         self.updater.start_webhook(
             listen='0.0.0.0',
             url_path=self.config.telegram_bot_token,
-            port=8443,
+            port=int(os.environ.get('PORT', 5000)),
             webhook_url=f'{self.config.host_url}/{self.config.telegram_bot_token}'
         )
         # self.updater.start_polling()

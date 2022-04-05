49f429a023f99ea2934068ac5d7ded24e4cc960d
Glebanister <glebmar2001@gmail.com>
2022-04-05 15:00:43 +0300

    h

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: bash ./ua_help/bot_students/run_release.sh
--- ua_help/bot_students/run_test.sh
+++ ua_help/bot_students/run_test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-mkdir -p /app/clients_data/v1/test
-bash -c 'python main.py --root /app/ua_help/bot_students/ --config /app/ua_help/bot_students/resources/config_test.json'
+mkdir -p /root/clients_data/v1/release
+bash -c 'python main.py --root /root/ukraine-help/ua_help/bot_students/ --config /root/ukraine-help/ua_help/bot_students/resources/config_test.json'
